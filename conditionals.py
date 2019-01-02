6f8cc1f30a54cd4e9b89737188afbb3dd0305b45
Efe Emir Pekmez <efeemir103@gmail.com>
2019-01-02 21:24:24 +0300

    Create conditionals.py

---
+++ conditionals.py
@@ -0,0 +1,11 @@
+print("What mark did you get from exam?")
+mark = float(input())
+if mark==100:
+  print("Congrats you got the full mark!!! :D ")
+elif mark>80:
+  print("Excellent! :) ")
+elif mark>35:
+  print("You passed :) ")
+else:
+  print("You failed :( ")
+print("Bye Bye :) ")
