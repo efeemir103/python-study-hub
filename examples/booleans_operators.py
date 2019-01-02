04cce1413826754b4117fc0480b696b7751cd8cd
Efe Emir Pekmez <efeemir103@gmail.com>
2019-01-02 21:32:08 +0300

    Create booleans_operators.py

---
+++ examples/booleans_operators.py
@@ -0,0 +1,11 @@
+print(True or False)
+print(True or True)
+print(False or True)
+print(True and True)
+print(False and True)
+print((True and False) or True)
+print(8==2)
+print(9==9)
+print((9<0) or (8>2))
+print((8>=0) or (2<=9))
+# try to guess the outputs
