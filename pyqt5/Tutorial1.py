3440ce40cbc5e92daa7c9d154f48538f63f52aab
efeemir103 <efeemir103@gmail.com>
2019-10-18 01:03:19 +0300

    Added some comments.

--- pyqt5/Tutorial1.py
+++ pyqt5/Tutorial1.py
@@ -3,16 +3,16 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
 def window():
-    app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(200, 200, 300, 300)
-    win.setWindowTitle("Test Window 1 Title")
+    app = QApplication(sys.argv) # get arguments from system
+    win = QMainWindow() # Call to our class
+    win.setGeometry(200, 200, 300, 300) # xpos, ypos, width, height
+    win.setWindowTitle("Test Window 1 Title") # title bar
 
     label = QtWidgets.QLabel(win)
     label.setText("my first label!")
     label.move(50, 50)
 
-    win.show()
-    sys.exit(app.exec_())
+    win.show() # show the window
+    sys.exit(app.exec_()) # check if qt window exited then exit backend process.
 
 window()
