1fb1a15ae45942a93b6ae3f0465aebc163f5634c
efeemir103 <efeemir103@gmail.com>
2019-10-23 00:46:21 +0300

    Finished tutorial 4

--- pyqt5/Tutorial4.py
+++ pyqt5/Tutorial4.py
@@ -12,17 +12,20 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
+        # Setting window
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        # Setting label
         self.label = QtWidgets.QLabel(self.centralwidget)
+        MainWindow.setCentralWidget(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(150, 110, 401, 291))
         font = QtGui.QFont()
         font.setPointSize(32)
         self.label.setFont(font)
         self.label.setObjectName("label")
-        MainWindow.setCentralWidget(self.centralwidget)
+        # Setting menubar
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 20))
         self.menubar.setObjectName("menubar")
@@ -31,9 +34,11 @@ class Ui_MainWindow(object):
         self.menuEdit = QtWidgets.QMenu(self.menubar)
         self.menuEdit.setObjectName("menuEdit")
         MainWindow.setMenuBar(self.menubar)
+        # Setting statubar
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
+        # Setting actions for menu
         self.actionNew = QtWidgets.QAction(MainWindow)
         self.actionNew.setObjectName("actionNew")
         self.actionSave = QtWidgets.QAction(MainWindow)
@@ -52,7 +57,18 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+        # Linking up some events to make buttons work
+        self.actionNew.triggered.connect( \
+            lambda: self.clicked("New was clicked."))
+        self.actionSave.triggered.connect( \
+            lambda: self.clicked("Save was clicked."))
+        self.actionCopy.triggered.connect( \
+            lambda: self.clicked("Copy was clicked."))
+        self.actionPaste.triggered.connect( \
+            lambda: self.clicked("Paste was clicked."))
+
     def retranslateUi(self, MainWindow):
+        # Setting up texts, titles, status tips and shortcuts.
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.label.setText(_translate("MainWindow", "TextLabel"))
@@ -71,12 +87,16 @@ class Ui_MainWindow(object):
         self.actionPaste.setStatusTip(_translate("MainWindow", "Paste a file"))
         self.actionPaste.setShortcut(_translate("MainWindow", "Ctrl+V"))
 
+    def clicked(self, text):
+        # Just a function to handle events.
+        self.label.setText(text)
+        self.label.adjustSize()
 
 if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
+    import sys # will be used to wait for application signal to exit.
+    app = QtWidgets.QApplication(sys.argv) # Get arguments passed.
+    MainWindow = QtWidgets.QMainWindow() # Set window.
+    ui = Ui_MainWindow() # Call to our class.
+    ui.setupUi(MainWindow) # Call to our method of the class we created.
     MainWindow.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec_()) # wait for gui exit signal.
